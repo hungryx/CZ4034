2cc901860939365829936dc859defbb0cee58e0e
wongdayou <64577096+wongdayou@users.noreply.github.com>
2024-04-10 00:46:37 +0800

    Updated application.py

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 import os
+
 def main():
     print('hello')
-    # cd to solr  folder
-    os.system('/solr/solr-9.2.1/bin/solr.cmd start -p 9893')
+    # cd to solr  folder\
+    current_directory = os.getcwd()
+    solr_directory = current_directory + r"\solr\solr-9.2.1\bin"
+    os.system(solr_directory + "\solr.cmd start -p 9893")
     # start solr
 
     # cd to frontend folder
