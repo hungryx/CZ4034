6a4104a96692ee74903ca312d9ede93986c7d509
wongdayou <64577096+wongdayou@users.noreply.github.com>
2024-04-10 15:28:27 +0800

    Fixed solr core issue
    Modified application.py to set solr_home to be user's working directory to prevent conflicts

--- application.py
+++ application.py
@@ -4,6 +4,9 @@ def main():
     print('hello')
     # cd to solr  folder\
     current_directory = os.getcwd()
+    env_directory = current_directory + r"\solr\solr-9.2.1"
+    os.system("set solr_home " + env_directory)
+
     solr_directory = current_directory + r"\solr\solr-9.2.1\bin"
     os.system(solr_directory + "\solr.cmd start -p 9893")
     # start solr
