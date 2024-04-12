7ab5d80f746acae38065bcc599eb1437e206ac55
wongdayou <64577096+wongdayou@users.noreply.github.com>
2024-04-12 12:57:48 +0800

    included sentiments in apache solr

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ def main():
     os.system("set solr_home " + env_directory)
 
     solr_directory = current_directory + r"\solr\solr-9.2.1\bin"
-    os.system(solr_directory + "\solr.cmd start -p 9893")
+    os.system(solr_directory + "\solr.cmd start -p 8983")
     # start solr
 
     # cd to frontend folder
